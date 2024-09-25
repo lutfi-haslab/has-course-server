5a516d27566475c9991c644165fe0d2a11fd0778
HY4-MAC-002 <hy4-mac-002@HY4-MAC-002.local>
2024-09-25 08:26:58 +0700

    move to native bun

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,4 @@
 import { Hono } from 'hono'
-import { handle } from 'hono/vercel'
-
-
 
 const app = new Hono().basePath('/api')
 
@@ -16,14 +13,6 @@ app.get('/hello', (c) => {
     return c.json({ message: "API throw error test" });
   }
 })
-
-const handler = handle(app);
-
-export const GET = handler;
-export const POST = handler;
-export const PATCH = handler;
-export const PUT = handler;
-export const OPTIONS = handler;
 
 export default {
   port: 3000,
